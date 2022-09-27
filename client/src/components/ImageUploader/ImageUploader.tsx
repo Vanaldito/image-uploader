9eaa899ba2dbf1bd6a367fc0a3a454c86faa8992
Vanaldito <fredimanuelb@gmail.com>
2022-09-26 20:10:26 -0600

    feat: allow upload an image with drag and drop

--- client/src/components/ImageUploader/ImageUploader.tsx
+++ client/src/components/ImageUploader/ImageUploader.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 import "./ImageUploader.css";
 
 export default function ImageUploader() {
+  const [image, setImage] = useState<DataTransferItem | null>(null);
+
+  function dropHandler<T>(event: React.DragEvent<T>) {
+    event.preventDefault();
+
+    if (event.dataTransfer.items.length === 0) return;
+
+    if (event.dataTransfer.items[0].type.startsWith("image/")) {
+      setImage(event.dataTransfer.items[0]);
+    }
+  }
+
   return (
     <div className="image-uploader">
       <h2 className="image-uploader__title">Upload your image</h2>
       <div className="image-uploader__restrictions">
         File should be Jpeg, Png,...
       </div>
-      <div className="image-uploader__drop">
+      <div
+        className="image-uploader__drop"
+        onDragOver={event => event.preventDefault()}
+        onDrop={dropHandler}
+      >
         <img src="/image.svg" />
-        <span>Drag & Drop your image here</span>
+        <span draggable>Drag & Drop your image here</span>
       </div>
       <div className="image-uploader__or">Or</div>
       <button className="image-uploader__choose-file-button">

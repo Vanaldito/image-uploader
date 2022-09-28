7d6abd84b2417d0bd75a2cc47fe6b69f097bd179
Vanaldito <fredimanuelb@gmail.com>
2022-09-28 11:00:34 -0600

    feat: add a Loader component

---
+++ client/src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      Uploading...
+      <div className="loader" />
+    </div>
+  );
+}

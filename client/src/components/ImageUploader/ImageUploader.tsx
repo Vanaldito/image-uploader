f191e83f888ef71eccd7eb79673d9e6ea47b334b
Vanaldito <fredimanuelb@gmail.com>
2022-09-28 18:55:30 -0600

    feat: use useFetchAndLoad hook to display the loader while the image is uploading

--- client/src/components/ImageUploader/ImageUploader.tsx
+++ client/src/components/ImageUploader/ImageUploader.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
+import { useFetchAndLoad } from "../../hooks";
 import { uploadImage } from "../../services";
+import { Loader } from "../Loader";
 
 import "./ImageUploader.css";
 
 export default function ImageUploader() {
-  const [image, setImage] = useState<File | null>(null);
+  const { loading, callEndpoint } = useFetchAndLoad();
 
   const fileInputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (!image) return;
-
-    uploadImage(image);
-  }, [image]);
 
   function dropHandler<T>(event: React.DragEvent<T>) {
     event.preventDefault();
 
     if (event.dataTransfer.items.length === 0) return;
+    if (!event.dataTransfer.items[0].type.startsWith("image/")) return;
 
-    if (event.dataTransfer.items[0].type.startsWith("image/")) {
-      setImage(event.dataTransfer.items[0].getAsFile());
-    }
+    const image = event.dataTransfer.items[0].getAsFile();
+
+    if (!image) return;
+
+    callEndpoint({ call: uploadImage(image) });
   }
 
   function changeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     if (!event.target.files || event.target.files.length === 0) return;
+    if (!event.target.files[0].type.startsWith("image/")) return;
 
-    if (event.target.files[0].type.startsWith("image/")) {
-      setImage(event.target.files[0]);
-    }
+    const image = event.target.files[0];
+
+    callEndpoint({ call: uploadImage(image) });
   }
 
   function clickHandler() {
@@ -38,27 +38,38 @@ export default function ImageUploader() {
 
   return (
     <div className="image-uploader">
-      <h2 className="image-uploader__title">Upload your image</h2>
-      <div className="image-uploader__restrictions">
-        File should be Jpeg, Png,...
-      </div>
-      <div
-        className="image-uploader__drop"
-        onDragOver={event => event.preventDefault()}
-        onDrop={dropHandler}
-      >
-        <img src="/image.svg" />
-        <span draggable>Drag & Drop your image here</span>
-      </div>
-      <div className="image-uploader__or">Or</div>
-      <button
-        className="image-uploader__choose-file-button"
-        type="button"
-        onClick={clickHandler}
-      >
-        Choose a file
-      </button>
-      <input ref={fileInputRef} type="file" hidden onChange={changeHandler} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h2 className="image-uploader__title">Upload your image</h2>
+          <div className="image-uploader__restrictions">
+            File should be Jpeg, Png,...
+          </div>
+          <div
+            className="image-uploader__drop"
+            onDragOver={event => event.preventDefault()}
+            onDrop={dropHandler}
+          >
+            <img src="/image.svg" />
+            <span draggable>Drag & Drop your image here</span>
+          </div>
+          <div className="image-uploader__or">Or</div>
+          <button
+            className="image-uploader__choose-file-button"
+            type="button"
+            onClick={clickHandler}
+          >
+            Choose a file
+          </button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            hidden
+            onChange={changeHandler}
+          />
+        </>
+      )}
     </div>
   );
 }
